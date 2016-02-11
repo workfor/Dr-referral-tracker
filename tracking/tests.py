4d9079029e72887fadeacf6243b9c364ccd58c61
Alexander Notset <shi3a@yandex.ru>
2016-02-11 04:04:13 +0300

    Add several tests

--- tracking/tests.py
+++ tracking/tests.py
@@ -1,13 +1,7 @@
 from django.test import TestCase
 
-from tracking.models import Physician
-
 
 class SimpleTest(TestCase):
     def test_sum(self):
         s = 2 + 2
         self.assertTrue(s, 4)
-
-    # Remove it, if necessary
-    def test_export_from_sqlite(self):
-        self.assertTrue(Physician.objects.get(physician_name='Dr John'))
